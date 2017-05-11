af744cc94bbcf748f33c062c6cf2f38bfdc73764
cyw <weems1995@hotmail.com>
2017-05-11 23:52:42 +0800

    5.11

--- polls/views.py
+++ polls/views.py
@@ -8,16 +8,13 @@ from django.urls import reverse
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list' : latest_question_list}
+    return render(request,'polls/index.html',context)
 
 
 def detail(request,question_id):
     question = get_object_or_404(Question,pk = question_id)
-    return render(request,'polls/detail.html',{'question':question})
+    return render(request,'polls/detail.html',{'question': question})
 
 
 def vote(request,question_id):
