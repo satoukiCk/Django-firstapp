90c91e611e4cf8d2f2bc6df07701989cf3480280
cyw <weems1995@hotmail.com>
2017-05-14 18:50:09 +0800

    5.14

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Question
-admin.site.register(Question)
+# Register your models here.
+from .models import Question,Choice
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date Information', {'fields': ['pub_date'], 'classes':['collapse']})
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text','pub_date','was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+admin.site.register(Question,QuestionAdmin)
+admin.site.register(Choice)
+
--- polls/views.py
+++ polls/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render,get_object_or_404
-from django.template import loader
+from django.utils import timezone
 from .models import Question,Choice
+from django.views import generic
 # Create your views here.
 from django.http import HttpResponse,HttpResponseRedirect
 from django.urls import reverse
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+    def get_queryset(self):
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-def detail(request,question_id):
-    question = get_object_or_404(Question,pk = question_id)
-    return render(request,'polls/detail.html',{'question': question})
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte =timezone.now())
 
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question,pk=question_id)
@@ -31,9 +40,5 @@ def vote(request, question_id):
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results',args = (question.id,)))
 
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
 
 
